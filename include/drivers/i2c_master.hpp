bf361ff4f6c4112201f79ae7024a5ffd6b33757a
Masroor Khonkhodzhaev <masroor47@icloud.com>
2024-12-10 15:27:40 -0500

    add init to .cpp file

--- include/drivers/i2c_master.hpp
+++ include/drivers/i2c_master.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <cstdint>
+#include "esp_log.h"
 #include "driver/i2c.h"
 
 class i2c_master {
@@ -9,32 +10,20 @@ public:
         static i2c_master instance;
         return instance;
     };
-    bool init(gpio_num_t sda, gpio_num_t scl, uint32_t frequency) {
-        i2c_config_t conf = {
-            mode: I2C_MODE_MASTER,
-            sda_io_num: sda,
-            scl_io_num: scl,
-            sda_pullup_en: GPIO_PULLUP_ENABLE,
-            scl_pullup_en: GPIO_PULLUP_ENABLE,
-            master: {
-                clk_speed: frequency
-            }
-        };
-        
-        esp_err_t err = i2c_param_config(I2C_NUM_0, &conf);
-        if (err != ESP_OK) return false;
-
-        err = i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0);
-        return err == ESP_OK;
-    }
-    
+    bool init(gpio_num_t sda, gpio_num_t scl, uint32_t frequency);
     bool read_registers(uint8_t device_addr, uint8_t start_reg, uint8_t* data, size_t len);
     bool write_register(uint8_t device_addr, uint8_t reg, uint8_t data);
     
 private:
+    static constexpr const char* TAG = "I2C";
     i2c_port_t i2c_port_ = I2C_NUM_0;
+    bool initialized_ = false;
+    bool pins_configured_ = false;
     i2c_master() = default;
     ~i2c_master() {
         i2c_driver_delete(I2C_NUM_0);
     }
+    // Prevent copying
+    i2c_master(const i2c_master&) = delete;
+    i2c_master& operator=(const i2c_master&) = delete;
 };
