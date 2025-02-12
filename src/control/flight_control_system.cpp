f846e3635ab6d4f886b478fd464d5a9c64804f8d
Masroor Khonkhodzhaev <masroor47@icloud.com>
2025-02-11 20:18:26 -0500

    implement attitude and rate control tasks

--- src/control/flight_control_system.cpp
+++ src/control/flight_control_system.cpp
@@ -7,7 +7,7 @@
 #include <memory>
 
 #include "esp_log.h"
-
+#include "control/pid_controller.hpp"
 #include "drivers/i2c_master.hpp"
 #include "sensors/mpu_6050.hpp"
 #include "sensors/bmp_280.hpp"
@@ -336,6 +336,131 @@ void flight_control_system::control_task(void* param) {
     }
 }
 
+void flight_control_system::attitude_control_task(void* param) {
+    auto& system = *static_cast<flight_control_system*>(param);
+    TickType_t last_wake_time = xTaskGetTickCount();
+    const TickType_t period = pdMS_TO_TICKS(20);  // 50Hz
+    
+    while (true) {
+        // Get latest attitude estimate
+        attitude_estimate current_attitude;
+        if (xSemaphoreTake(system.attitude_data_.mutex, portMAX_DELAY) == pdTRUE) {;
+            current_attitude = system.attitude_data_.latest_estimate;
+            xSemaphoreGive(system.attitude_data_.mutex);
+        }
+        
+        // Run attitude PIDs
+        float desired_pitch_rate = system.yaw_attitude_pid_->update(
+            0.0, current_attitude.pitch, 0.02f);
+        float desired_yaw_rate = system.pitch_attitude_pid_->update(
+            0.0, current_attitude.roll, 0.02f);
+        
+        // Update rate setpoints
+        if (xSemaphoreTake(system.rate_setpoint_data_.mutex, portMAX_DELAY) == pdTRUE) {;
+            system.rate_setpoint_data_.setpoints.roll_rate = 0.0;
+            system.rate_setpoint_data_.setpoints.pitch_rate = desired_pitch_rate;
+            system.rate_setpoint_data_.setpoints.yaw_rate = desired_yaw_rate;
+            xSemaphoreGive(system.rate_setpoint_data_.mutex);
+        }
+        
+        vTaskDelayUntil(&last_wake_time, period);
+    }
+}
+
+// temporary function to simulate throttle ramp up and down
+float calculate_throttle(int elapsed_ms, float max_throttle = 25.0f) {
+   const int initial_delay = 8000;
+   const int ramp_up = 4000;
+   const int sustain = 30000;
+   const int ramp_down = 2000;
+   
+   if (elapsed_ms < initial_delay) return 0.0f;
+   if (elapsed_ms < initial_delay + ramp_up) {
+       float x = (float)(elapsed_ms - initial_delay) / ramp_up;
+       return max_throttle * (1.0f / (1.0f + exp(-10.0f * (x - 0.5f))));
+   }
+   if (elapsed_ms < initial_delay + ramp_up + sustain) return max_throttle;
+   if (elapsed_ms < initial_delay + ramp_up + sustain + ramp_down) {
+       float x = (float)(elapsed_ms - (initial_delay + ramp_up + sustain)) / ramp_down;
+       return max_throttle * (1.0f - x);
+   }
+   return 0.0f;
+}
+
+void flight_control_system::rate_control_task(void* param) {
+    auto& system = *static_cast<flight_control_system*>(param);
+    TickType_t last_wake_time = xTaskGetTickCount();
+    int tick = 0;
+    const int period_ms = 20;
+    const TickType_t period = pdMS_TO_TICKS(20);  // 50Hz
+
+    float throttle_percent;
+    
+    float test_angle = 0;
+
+    while (true) {
+        // Get latest rate setpoints
+        float roll_rate_sp, pitch_rate_sp, yaw_rate_sp;
+        if (xSemaphoreTake(system.rate_setpoint_data_.mutex, portMAX_DELAY) == pdTRUE) {;
+            roll_rate_sp = system.rate_setpoint_data_.setpoints.roll_rate;
+            pitch_rate_sp = system.rate_setpoint_data_.setpoints.pitch_rate;
+            yaw_rate_sp = system.rate_setpoint_data_.setpoints.yaw_rate;
+            xSemaphoreGive(system.rate_setpoint_data_.mutex);
+        }
+
+        // Get latest gyro data
+        mpu_6050::mpu_reading gyro_data;
+        xSemaphoreTake(system.imu_data_.mutex, portMAX_DELAY);
+        gyro_data = system.imu_data_.latest_reading;
+        xSemaphoreGive(system.imu_data_.mutex);
+        
+        roll_rate_sp = 0.0f;
+        pitch_rate_sp = 0.0f;
+        yaw_rate_sp = 0.0f;
+
+        // Run rate PIDs
+        float roll_thrust = system.roll_rate_pid_->update(
+            roll_rate_sp, gyro_data.gyro[1], 0.02f);
+
+        float pitch_thrust = system.pitch_rate_pid_->update(
+            pitch_rate_sp, gyro_data.gyro[0], 0.02f);
+
+        float yaw_thrust = system.yaw_rate_pid_->update(
+            yaw_rate_sp, gyro_data.gyro[2], 0.02f);
+
+        // Precession compensation
+        throttle_percent = calculate_throttle(++tick * period_ms);
+        system.esc_->set_throttle(throttle_percent);
+
+        float K_PRECESSION = 0.1f;  // Example precession gain
+        const float precession_gain = K_PRECESSION;// * throttle_percent;  // Experimentally determined
+        const float comp_pitch = pitch_thrust*(1-precession_gain) + precession_gain * yaw_thrust;
+        const float comp_yaw = yaw_thrust*(1-precession_gain) - precession_gain * pitch_thrust;
+
+        // Torque compensation
+        float K_TORQUE = 0.35f;  // Example torque gain
+        float counter_torque_angle = throttle_percent * K_TORQUE;
+        
+        const float servo1_cmd = -comp_pitch - comp_yaw + roll_thrust - counter_torque_angle;
+        const float servo2_cmd =  comp_pitch - comp_yaw + roll_thrust - counter_torque_angle;
+        const float servo3_cmd =  comp_pitch + comp_yaw + roll_thrust - counter_torque_angle;
+        const float servo4_cmd = -comp_pitch + comp_yaw + roll_thrust - counter_torque_angle;
+
+        ESP_LOGI(TAG, "motor: %2f%%, gyro: %.2f, %.2f, %.2f; pitch_th: %.2f, yaw_th: %.2f; roll_th: %.2f,  comp_pitch: %.2f, comp_yaw: %.2f",
+            throttle_percent, gyro_data.gyro[0], gyro_data.gyro[1], gyro_data.gyro[2], pitch_thrust, yaw_thrust, roll_thrust, comp_pitch, comp_yaw
+        );
+
+        test_angle = sin(tick * period_ms * 0.01f) * 45.0f;
+
+        system.servos_[0]->set_angle(test_angle);
+        system.servos_[1]->set_angle(test_angle);
+        system.servos_[2]->set_angle(test_angle);
+        system.servos_[3]->set_angle(test_angle);
+        
+        vTaskDelayUntil(&last_wake_time, period);
+    }
+}
+
 bool flight_control_system::start() {
 
     BaseType_t ret;
@@ -349,7 +474,6 @@ bool flight_control_system::start() {
         &imu_task_handle_,
         1
     );
-
     if (ret != pdPASS) {
         ESP_LOGE(TAG, "Failed to create IMU task");
         return false;
@@ -378,7 +502,6 @@ bool flight_control_system::start() {
         &barometer_task_handle_,
         1
     );
-
     if (ret != pdPASS) {
         ESP_LOGE(TAG, "Failed to create barometer task");
         return false;
@@ -399,18 +522,45 @@ bool flight_control_system::start() {
     // }
 
 
+    // ret = xTaskCreatePinnedToCore(
+    //     control_task,
+    //     "control_task",
+    //     4096,
+    //     this,
+    //     configMAX_PRIORITIES - 4,
+    //     &control_task_handle_,
+    //     0
+    // );
+    // if (ret != pdPASS) {
+    //     ESP_LOGE(TAG, "Failed to create control task");
+    //     return false;
+    // }
+
     ret = xTaskCreatePinnedToCore(
-        control_task,
-        "control_task",
+        attitude_control_task,
+        "attitude_control_task",
         4096,
         this,
         configMAX_PRIORITIES - 4,
-        &control_task_handle_,
+        &attitude_control_task_handle_,
         0
     );
-
     if (ret != pdPASS) {
-        ESP_LOGE(TAG, "Failed to create control task");
+        ESP_LOGE(TAG, "Failed to create attitude control task");
+        return false;
+    }
+
+    ret = xTaskCreatePinnedToCore(
+        rate_control_task,
+        "rate_control_task",
+        4096,
+        this,
+        configMAX_PRIORITIES - 4,
+        &rate_control_task_handle_,
+        0
+    );
+    if (ret != pdPASS) {
+        ESP_LOGE(TAG, "Failed to create rate control task");
         return false;
     }
 
