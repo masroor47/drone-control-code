90d13b11d0d9a458b9cb3c8ea499d97b18ddc725
Masroor Khonkhodzhaev <masroor47@icloud.com>
2025-03-14 23:54:22 -0400

    add systen initialization for quad

--- main/main.cpp
+++ main/main.cpp
@@ -2,21 +2,40 @@
 #include "esp_log.h"
 #include "driver/gpio.h"
 #include "control/flight_control_system.hpp"
+#include "control/quad_fcs.hpp"
 
 static const char* TAG = "main";
 
 extern "C" void app_main(void)
 {
-    static std::unique_ptr<flight_control_system> system;
+    // static std::unique_ptr<flight_control_system> system;
+    // flight_control_system::config config {
+    //     .i2c_sda = GPIO_NUM_21,
+    //     .i2c_scl = GPIO_NUM_22,
+    //     .i2c_freq = 400000,
+    //     .servo1_pin = GPIO_NUM_15,
+    //     .servo2_pin = GPIO_NUM_12,
+    //     .servo3_pin = GPIO_NUM_25,
+    //     .servo4_pin = GPIO_NUM_32,
+    //     .rc_receiver = {
+    //         .rx_pin = GPIO_NUM_16,
+    //         .tx_pin = GPIO_NUM_17,
+    //         .uart_num = UART_NUM_2,
+    //         .baud_rate = 420000
+    //     },
+    // };
+    // system = std::make_unique<flight_control_system>(config);
 
-    flight_control_system::config config {
+    // Quadcopter
+    static std::unique_ptr<quad_fcs> system;
+    quad_fcs::config config {
         .i2c_sda = GPIO_NUM_21,
         .i2c_scl = GPIO_NUM_22,
         .i2c_freq = 400000,
-        .servo1_pin = GPIO_NUM_15,
-        .servo2_pin = GPIO_NUM_12,
-        .servo3_pin = GPIO_NUM_25,
-        .servo4_pin = GPIO_NUM_32,
+        .motor1_pin = GPIO_NUM_15,
+        .motor2_pin = GPIO_NUM_12,
+        .motor3_pin = GPIO_NUM_25,
+        .motor4_pin = GPIO_NUM_32,
         .rc_receiver = {
             .rx_pin = GPIO_NUM_16,
             .tx_pin = GPIO_NUM_17,
@@ -25,7 +44,7 @@ extern "C" void app_main(void)
         },
     };
 
-    system = std::make_unique<flight_control_system>(config);
+    system = std::make_unique<quad_fcs>(config);
     
     if (!system->init()) {
         ESP_LOGE(TAG, "Failed to initialize flight control system");
